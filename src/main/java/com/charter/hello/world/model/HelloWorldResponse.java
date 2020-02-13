398ea73c946421c4580af98bfa56ab573006e44e
Jarred-PM <jarred.maestas@practicemojo.com>
2020-02-12 21:54:02 -0700

    Added comments relating to solution

--- src/main/java/com/charter/hello/world/model/HelloWorldResponse.java
+++ src/main/java/com/charter/hello/world/model/HelloWorldResponse.java
@@ -1,6 +1,7 @@
 package com.charter.hello.world.model;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+
 import lombok.Builder;
 import lombok.Data;
 
@@ -10,7 +11,22 @@ import lombok.Data;
 public class HelloWorldResponse {
     String message;
 
-    public HelloWorldResponse(String message){
-        this.message = message;
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	// had to add empty constructor method otherwise pointing to a null or empty
+	// constructor that was istantiated with the message field would not point
+	// to the same location and therefore could not be overwritten
+	public HelloWorldResponse() {
+	}
+
+	public HelloWorldResponse(String message) {
+		this.message = message;
     }
+
 }
--- src/main/java/com/charter/hello/world/web/WebController.java
+++ src/main/java/com/charter/hello/world/web/WebController.java
@@ -1,16 +1,41 @@
 package com.charter.hello.world.web;
 
-import com.charter.hello.world.model.HelloWorldResponse;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.charter.hello.world.model.HelloWorldResponse;
+
 @RestController
 public class WebController {
-    @GetMapping(value = "/hello",
-            consumes = {"application/json"},
-            produces = {"application/json"})
-    @ResponseBody HelloWorldResponse hello(){
-        return new HelloWorldResponse("Hello World!");
-    }
+
+	HelloWorldResponse response = new HelloWorldResponse();
+
+	// POST mapping, routed to "/hello"
+	@RequestMapping(value = "/hello", 
+			method = RequestMethod.POST,
+			consumes = { "application/json" }, 
+			produces = { "application/json"})
+	// create post method that accepts the json payload, since the class
+	// construction of the response already matches the json payload we can
+	// just set the istantiated response equal to the request
+	public void postMethod(
+			@RequestBody HelloWorldResponse request) {
+		response = request;
+	}
+
+	// GET mapping,S routed to "/hello"
+	@RequestMapping(value = "/hello", 
+			method = RequestMethod.GET, 
+            consumes = {"application/json"}, 
+			produces = { "application/json"})
+	// ResponseBody mapping, simply return the response which is the
+	// HelloWorldResponse we already istantiated and made equal to the
+	// in the postMethod
+	@ResponseBody
+	public HelloWorldResponse gettMethod() {
+		return response;
+	}
 }
